c985a93e621d3088abe10f6fb9c697cf4453c7ea
asf0 <scorpionspfc@gmail.com>
2026-02-15 21:54:10 +0000

    fixed config.ts

--- frontend/lib/config.ts
+++ frontend/lib/config.ts
@@ -3,4 +3,4 @@ export const BACKEND_URL = process.env.BACKEND_URL ||
                            process.env.NEXT_PUBLIC_API_URL || 
                            "http://localhost:8000";
 
-export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "https://jobfinder.asf0.dev"; #
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "https://jobfinder.asf0.dev"; 
